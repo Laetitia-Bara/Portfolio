9f0d36586390f16f9eaf2b8703b400e6ae5b6d12
Laëtitia BARA <bara.laetitia@gmail.com>
2026-06-06 19:03:12 +0200

    add ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Letty / Portfolio",
@@ -15,6 +16,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+
+<head>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5057499390168934"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+    </head>
+
       <body className="text-zinc-100 antialiased">
         <Header />
         <main className="min-h-[calc(100vh-56px)]">{children}</main>
